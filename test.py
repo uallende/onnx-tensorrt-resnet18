c6c0dcdf20084e850b4e75319ecf4fb6c8ffb278
root <allende.rev@gmail.com>
2024-04-05 10:40:17 +0000

    torch preprocessing works

--- test.py
+++ test.py
@@ -7,11 +7,17 @@ import torchvision.transforms as v1
 import matplotlib.pyplot as plt
 
 path_to_original_imgs = f'resnet18/images/'
+labels_path = f'resnet18/imagenet-classes.txt'
 original_img_list = os.listdir(path_to_original_imgs)
 
+with open(labels_path, 'r') as f:
+    labels = [line.strip() for line in f]
 
 img_list = []
 img_path = []
+
+model = torchvision.models.resnet18(weights='IMAGENET1K_V1')
+model.eval()
 
 for img in original_img_list:
 
@@ -22,31 +28,24 @@ for img in original_img_list:
         img = image.resize((244,244), Image.NEAREST)
         img_np = np.array(img).astype(np.float32) / 255.0
         img_np = np.expand_dims(img_np, axis=0)
-        # print(img_np[:1])
         img_list.append(img_np)
         img_path.append(full_path)
-        break
 
+        # break
 
-model = torchvision.models.resnet18(weights='IMAGENET1K_V1')
+for image_path in img_path:
 
-image1 = read_image(img_path[0])
-transforms = torch.nn.Sequential(
-    v1.RandomCrop(224),
-    v1.RandomHorizontalFlip(p=0.1)
-)
+    image = read_image(image_path) / 255
+    transforms = torch.nn.Sequential(
+        v1.Resize((224,224)),
+        v1.RandomHorizontalFlip(p=0.1)
+    )
 
-scripted_transforms = torch.jit.script(transforms)
-image1 = scripted_transforms(image1).float().unsqueeze(dim=0)
-
-
-print(image1.dtype)
-print(image1.shape)
-
-
-output = model(image1)
-# print(model)
-print(torch.argmax(output))
+    scripted_transforms = torch.jit.script(transforms)
+    image = scripted_transforms(image).float().unsqueeze(dim=0)
+    output = model(image)
+    class_output = torch.argmax(output)
+    print(f'{image_path} -------- {labels[class_output]}')
 
 
 # NOT NEEDED
